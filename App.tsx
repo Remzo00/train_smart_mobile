a44d45b6e5073879b592c97d2afb5d498f9b93c1
Remzo00 <gusinacremzo00@gmail.com>
2024-05-20 11:44:51 +0200

    Sign up screen

--- App.tsx
+++ App.tsx
@@ -1,37 +1,6 @@
-import React, { useState } from "react";
-import { View } from "react-native";
-import Button from "./components/Button";
-import InputField from "./components/InputField";
+import React from "react";
+import SignUp from "./screens/SignUp";
 
 export default function App() {
-  const [text, setText] = useState<string>("");
-  const [error, setError] = useState<string>("");
-
-  const handleChange = (inputText: string) => {
-    setText(inputText);
-    if (inputText === "") {
-      setError("Field cannot be empty");
-    } else {
-      setError("");
-    }
-  };
-
-  return (
-    <View style={{ padding: 50 }}>
-      <Button
-        text={"Neki tekst"}
-        onPress={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        color="blue"
-      />
-
-      <InputField
-        value={text}
-        onChange={handleChange}
-        placeholder="Unesite tekst"
-        error={error}
-      />
-    </View>
-  );
+  return <SignUp />;
 }
---
+++ screens/SignUp/index.styeld.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    padding: 20px;
+    background-color: white;
+`;
+
+export const Title = styled.Text`
+    font-size: 32px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 20px;
+    margin-bottom: 10px;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 18px;
+  text-align: center;
+  margin-bottom: 30px;
+  color: #666;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-top: 20px;
+  align-items: center;
+`;
